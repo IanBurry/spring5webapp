afa32634cf577f83f4fdb708b624c33ca4cac441
Ian Burry <iburry@aol.com>
2019-07-05 16:24:03 -0700

    Add Publisher one-to-one for Book

--- src/main/java/home/springthing/demo/bootstrap/DevBootstrap.java
+++ src/main/java/home/springthing/demo/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package home.springthing.demo.bootstrap;
 
 import home.springthing.demo.model.Author;
 import home.springthing.demo.model.Book;
+import home.springthing.demo.model.Publisher;
 import home.springthing.demo.repositories.AuthorRepository;
 import home.springthing.demo.repositories.BookRepository;
+import home.springthing.demo.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,12 @@ import org.springframework.stereotype.Component;
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -24,17 +28,21 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+        Publisher harperCollins = new Publisher("Harper Collins", "1234 Harper Collins Ave.");
+        Publisher wrox = new Publisher("Wrox");
+        publisherRepository.save(harperCollins);
+        publisherRepository.save(wrox);
 
         // Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins" );
+        Book ddd = new Book("Domain Driven Design", "1234", harperCollins);
         ddd.getAuthors().add(eric);
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
         // Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", wrox);
         rod.getBooks().add(noEJB);
         authorRepository.save(rod);
         bookRepository.save(noEJB);
--- src/main/java/home/springthing/demo/model/Book.java
+++ src/main/java/home/springthing/demo/model/Book.java
@@ -13,7 +13,9 @@ public class Book {
     private long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -23,12 +25,12 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String publisher) {
+    public Book(String title, Publisher publisher) {
         this.title = title;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -50,11 +52,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
