c1c0e69d3aee655bdca41c367ed974ad8853560d
Ian Burry <iburry@aol.com>
2019-07-05 18:02:14 -0700

    Add authors list view

--- src/main/java/home/springthing/demo/model/Book.java
+++ src/main/java/home/springthing/demo/model/Book.java
@@ -40,6 +40,14 @@ public class Book {
         return title;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
